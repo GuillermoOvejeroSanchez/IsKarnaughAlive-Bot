a7f6813f4126c4e142040e75b87126b153fdacb5
GuillermoOvejeroSanchez <guilleov@ucm.es>
2019-03-26 18:56:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,30 +7,33 @@ var request = require("request");
 
 var alreadyDead = false;
 var diedOn = "DD/MM/YYYY";
-
+var status = '';
+var statusIndex = 0;
 tweetKarnaughStatus();
 setInterval(tweetKarnaughStatus, 1000 * 60 * 60 * 12); //tweets every 12 hours
 
 function tweetKarnaughStatus() {
-    var isAlive = getKarnaughStatus();
-    var status = '';
-    if (isAlive)
-        status = 'YES';
-    else if (!isAlive) {
+    var isDead = getKarnaughStatus();
+    if (!isDead)
+        status = `YES at ${moment().format('MMMM Do YYYY, h:mm:ss a')}`; //Diferent status, else throws error
+    else if (isDead) {
         if (!alreadyDead) {
             diedOn = moment().format("DD/MM/YYYY");
         }
         status = `REST IN PEPPERONI KARNAUGH ${diedOn}`;
         alreadyDead = true;
     }
+    statusIndex++;
 
-    //Post tweet with status
+/*
     T.post('statuses/update', {
         status: status
     }, function (err, data, response) {
         if (err) console.log(err);
     })
+*/
 }
+
 
 function getKarnaughStatus() {
 
@@ -47,7 +50,7 @@ function getKarnaughStatus() {
             var parsed = JSON.parse(body);
             var infobox = parsed.query.pages[12748221].revisions[0]["*"].split('|');
 
-            // console.log(`${infobox[14]}${infobox[15]}${infobox[16]}${infobox[17]}${infobox[18]}${infobox[19]}${infobox[20]}`); //Print death_date
+            //console.log(`${infobox[14]}${infobox[15]}${infobox[16]}${infobox[17]}${infobox[18]}${infobox[19]}${infobox[20]}`); //Print death_date
 
             if (infobox[14] != ' death_date  =   <!--{{death date and age ') {
                 //console.log('rip');
